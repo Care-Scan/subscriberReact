57b6be2608a1f7f508bcd2017ad681b03b945ddc
jae <albertforus@gmail.com>
2023-02-12 16:44:19 -0500

    data fetch and populate form; demo version

--- src/components/EditArea.js
+++ src/components/EditArea.js
@@ -1,45 +1,60 @@
 import React from "react";
 
 export default function EditArea() {
-  const [data, setData] = React.useState("state");
+  const [data, setData] = React.useState("Digitized Patient Intake Form");
   React.useEffect(() => {
     fetch("http://localhost:3001/api")
       .then((res) => res.json())
-      .then((data) => setData(data.message));
+      .then((data) => setData(data));
   }, []);
 
   return (
     <div className="edit-area split-screen">
-      <h1>{data}</h1>
+      <h1>Digitized Patient Intake Form</h1>
       <form>
         <ul className="form-list">
           <li>
-            <label htmlFor="first-name">First Name</label>
-            <input type="text" id="first-name" placeholder="Enter patient's first name here" />
-          </li>
-          <li>
-            <label htmlFor="last-name">Last Name</label>
-            <input type="text" id="last-name" placeholder="Enter patient's last name here" />
+            <label htmlFor="first-name">Patient Name</label>
+            <input type="text" id="first-name" defaultValue={data["Patient name"]} />
           </li>
           <li>
             <label htmlFor="dob">Date of Birth</label>
-            <input type="date" id="dob" placeholder="Enter patient's date of birth here" />
+            <input type="text" id="dob" defaultValue={data["Date of birth"]} />
+          </li>
+          <li>
+            <label htmlFor="province">Marital Status</label>
+            <input type="text" id="marriage" defaultValue={data["Marital status"]} />
+          </li>
+          <li>
+            <label htmlFor="province">Country of Birth</label>
+            <input type="text" id="cob" defaultValue={data["Country of birth"]} />
+          </li>
+          <li>
+            <label htmlFor="province">Current Occupation</label>
+            <input type="text" id="occupation" defaultValue={data["Current occupation"]} />
+          </li>
+          <li>
+            <label htmlFor="province">Email Address</label>
+            <input type="text" id="email" defaultValue={data["Email address"]} />
           </li>
           <li>
             <label htmlFor="address">Address</label>
-            <input type="text" id="address" placeholder="Enter patient's address here" />
+            <input type="text" id="address" defaultValue={data["Address"]} />
           </li>
           <li>
             <label htmlFor="city">City</label>
-            <input type="text" id="city" placeholder="Enter patient's city here" />
+            <input type="text" id="city" defaultValue={data["City"]}
+                  placeholder="Enter patient's city of residence" />
           </li>
           <li>
             <label htmlFor="province">Province</label>
-            <input type="text" id="province" placeholder="Enter patient's province here" />
+            <input type="text" id="province" defaultValue={data["Province"]} />
           </li>
+
           <li>
             <label htmlFor="phone-number">Phone Number</label>
-            <input type="text" id="phone-number" placeholder="Enter patient's phone number here" />
+            <input type="text" id="phone-number" defaultValue={data["Phone #"]}
+                    placeholder="Enter patient's phone number here" />
           </li>
           <li>
             <label htmlFor="health-card-number">Health Card Number</label>
@@ -50,17 +65,10 @@ export default function EditArea() {
             <label htmlFor="message">Medical Conditions</label>
             <textarea rows="6" id="medical-conditions" placeholder="Medical Conditions"></textarea>
           </li>
-          <li>
-            <label htmlFor="diabetes">Diabetes</label>
-            {/* <select id="diabetes">
-              <option value="" selected="selected"> </option>
-              <option value="yes">Yes</option>
-              <option value="no">No</option>
-            </select> */}
-          </li>
+
           <li>
             <label htmlFor="diabetes-details">Family Memeber and Age at Diagnosis</label>
-            <textarea rows="3" id="diabetes-details" placeholder="Diabetes Details"></textarea>
+            <textarea rows="3" id="diabetes-details" placeholder="Family Details"></textarea>
           </li>
           <li>
             <button type="submit" class="btn btn-lightblue btn-animate">Submit</button>
